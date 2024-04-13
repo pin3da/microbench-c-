08448dd947c352d11cbb76c2f49a64cdcd7b9756
Manuel Pineda <manuelpineda@google.com>
2024-04-13 16:52:35 +0200

    Add StrCat microbenchmark

--- README.md
+++ README.md
@@ -14,6 +14,17 @@ Requires https://bazel.build/ to be installed.
 
 `bazel run //main:main -c opt `
 
+This will generate an output like the following:
+
+```
+------------------------------------------------------------
+Benchmark                  Time             CPU   Iterations
+------------------------------------------------------------
+BM_StringConcatV1     519933 ns       518423 ns         1000
+BM_StringConcatV2    1717999 ns      1703354 ns          413
+```
+
+
 -----
 
 https://github.com/pin3da https://gitlab.com/pin3da
--- main/main.cc
+++ main/main.cc
@@ -1,18 +1,66 @@
+#include <iostream>
+#include <random>
+#include <string>
+#include <string_view>
+
+// Bazel-dependency
 #include <benchmark/benchmark.h>
 
-static void BM_StringCreation(benchmark::State& state) {
-  for (auto _ : state)
-    std::string empty_string;
-}
-// Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
+std::string GenerateRandomString(int length) {
+  std::string_view charset =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> dis(0, charset.size() - 1);
 
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-  std::string x = "hello";
-  for (auto _ : state)
-    std::string copy(x);
+  std::string random_string;
+  random_string.reserve(length);
+
+  for (int i = 0; i < length; ++i) {
+    random_string += charset[dis(gen)];
+  }
+
+  return random_string;
 }
-BENCHMARK(BM_StringCopy);
+
+static std::vector<std::string> StringsToConcat() {
+  int num_strings = 1000;
+  int max_length = 1000;
+  std::vector<std::string> ret(num_strings);
+  for (int i = 0; i < num_strings; ++i) {
+    // TODO: Assign random lenght up to max_length.
+    ret[i] = GenerateRandomString(max_length);
+  }
+  return ret;
+}
+
+static void BM_StringConcatV1(benchmark::State &state) {
+  for (auto _ : state) {
+    state.PauseTiming();
+    auto data = StringsToConcat();
+    state.ResumeTiming();
+    std::string result;
+    for (auto &s : data) {
+      result += s;
+    }
+  }
+}
+BENCHMARK(BM_StringConcatV1);
+
+static void BM_StringConcatV2(benchmark::State &state) {
+  for (auto _ : state) {
+    state.PauseTiming();
+    auto data = StringsToConcat();
+    state.ResumeTiming();
+    std::string result;
+
+    for (auto &s : data) {
+      for (auto &c : s) {
+        result.push_back(c);
+      }
+    }
+  }
+}
+BENCHMARK(BM_StringConcatV2);
 
 BENCHMARK_MAIN();
